85f5cf997b41a22d2485d087672be3726c362400
Nate <nategamedevelopmentacc@gmail.com>
2022-02-04 22:58:18 -0800

    add xmalloc and xcalloc

---
+++ include/common.c
@@ -0,0 +1,15 @@
+#include "common.h"
+
+void *xcalloc(size_t multiplier, size_t size){
+    void *result = calloc(multiplier, size);
+    if(result == NULL)
+        ERROR("error: unable to allocate memory!");
+    return result;
+}
+
+void *xmalloc(size_t size){
+    void *result = malloc(size);
+    if(result == NULL)
+        ERROR("error: unable to allocate memory!");
+    return result;
+}
