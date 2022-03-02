ce21ac186eb7547b1aa03003932e7040c7add634
Nathaniel <nategamedevelopmentacc@gmail.com>
2022-03-01 20:43:28 -0800

    suppress struct warning message

--- Server/include/account.h
+++ Server/include/account.h
@@ -2,9 +2,11 @@
 
 #include "../../include/common.h"
 #include "../server.h"
-#include <stdio.h>
 #include <pthread.h>
+#include <stdio.h>
 
 #define FOUND_UID_AND_PASS 0
 
-int account_load(struct client *cli, char *datapath, char* uid, char *paswd);
+struct client;
+
+int account_load(struct client *cli, char *datapath, char *uid, char *paswd);
--- Server/src/account.c
+++ Server/src/account.c
@@ -1,70 +1,70 @@
 #include "../include/account.h"
 
-int account_load(struct client *cli, char *datapath, char *uid, char *paswd){
-    FILE *fp = NULL;
-    char ch = '\0';
-    size_t bufsize = 0;
-    char *uid_buf = NULL;
-    char *linebuf = NULL;
-    fpos_t fpos;
+int account_load(struct client *cli, char *datapath, char *uid, char *paswd) {
+  FILE *fp = NULL;
+  char ch = '\0';
+  size_t bufsize = 0;
+  char *uid_buf = NULL;
+  char *linebuf = NULL;
+  fpos_t fpos;
 
-    for(int i = 0; i < MAX_CLIENTS; ++i){
-        if(clients[i]){
-            if(clients[i]->uid == atoi(uid)){
-                printf("Client already logged in\n");
-                return EXIT_FAILURE;
-            }
-        }
+  for (int i = 0; i < MAX_CLIENTS; ++i) {
+    if (clients[i]) {
+      if (clients[i]->uid == atoi(uid)) {
+        printf("Client already logged in\n");
+        return EXIT_FAILURE;
+      }
     }
+  }
 
-    if(!(fp = fopen(datapath, "r")))
-        ERROR("Cannot open account data file!\n");
+  if (!(fp = fopen(datapath, "r")))
+    ERROR("Cannot open account data file!\n");
 
-    while(!feof(fp)){    //while not at the end of file
-        fsetpos(fp, &fpos);
-        fgetpos(fp, &fpos);
+  while (!feof(fp)) { // while not at the end of file
+    fsetpos(fp, &fpos);
+    fgetpos(fp, &fpos);
 
-        for(bufsize = 0; (ch = fgetc(fp)) != '\n'; bufsize++);
+    for (bufsize = 0; (ch = fgetc(fp)) != '\n'; bufsize++)
+      ;
 
-        fsetpos(fp, &fpos);
+    fsetpos(fp, &fpos);
 
-        linebuf = xcalloc(bufsize, sizeof(char));
-        getline(&linebuf, &bufsize, fp);
-        linebuf[strcspn(linebuf, "\n")] = '\0';
+    linebuf = xcalloc(bufsize, sizeof(char));
+    getline(&linebuf, &bufsize, fp);
+    linebuf[strcspn(linebuf, "\n")] = '\0';
 
-        fgetpos(fp, &fpos);
+    fgetpos(fp, &fpos);
 
-        if(strcmp(linebuf, uid) == 0){
-            printf("Found Your UID! %s\n", linebuf);
+    if (strcmp(linebuf, uid) == 0) {
+      printf("Found Your UID! %s\n", linebuf);
 
-            uid_buf = xmalloc(sizeof(linebuf));
-            strncpy(uid_buf, linebuf, strlen(linebuf));
+      uid_buf = xmalloc(sizeof(linebuf));
+      strncpy(uid_buf, linebuf, strlen(linebuf));
 
-            free(linebuf);
-            fgetpos(fp, &fpos);
-            for(bufsize = 0; (ch = fgetc(fp)) != '\n'; bufsize++);
-            fsetpos(fp, &fpos);
+      free(linebuf);
+      fgetpos(fp, &fpos);
+      for (bufsize = 0; (ch = fgetc(fp)) != '\n'; bufsize++)
+        ;
+      fsetpos(fp, &fpos);
 
-            linebuf = xcalloc(bufsize, sizeof(char));
-            getline(&linebuf, &bufsize, fp);
+      linebuf = xcalloc(bufsize, sizeof(char));
+      getline(&linebuf, &bufsize, fp);
 
-            linebuf[strcspn(linebuf, "\n")] = '\0';
+      linebuf[strcspn(linebuf, "\n")] = '\0';
 
-            if(strcmp(linebuf, paswd) == 0){
-                cli->uid = atoi(uid_buf);
-                printf("Your uid %d\n", cli->uid);
-                printf("Found your password, %s\n", linebuf);
-                return FOUND_UID_AND_PASS;
-            }
-            else{
-                free(uid_buf);
-                printf("You entered the wrong password\n");
-                return EXIT_FAILURE;
-            }
-        }
-        else{
-            printf("UID not found yet, %s\n", linebuf);
-        }
+      if (strcmp(linebuf, paswd) == 0) {
+        cli->uid = atoi(uid_buf);
+        printf("Your uid %d\n", cli->uid);
+        printf("Found your password, %s\n", linebuf);
+        return FOUND_UID_AND_PASS;
+      } else {
+        free(uid_buf);
+        printf("You entered the wrong password\n");
+        return EXIT_FAILURE;
+      }
+    } else {
+      printf("UID not found yet, %s\n", linebuf);
     }
-    return EXIT_FAILURE;
+  }
+  return EXIT_FAILURE;
 }
